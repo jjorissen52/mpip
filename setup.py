4fa2c82e8ac9e068b68f313f64c2700cf02db1d8
jjorissen52 <jjorissen52@gmail.com>
2021-01-06 11:35:17 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="mpip",
+    version='0.1.0',
+    license='MIT',
+    url="https://github.com/jjorissen52/mpip",
+
+    description="mpip is a thin wrapper around pip which helps you manage your project requirements.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    author='John-Paul Jorissen',
+    author_email='jjorissen52@gmail.com',
+
+    keywords='python pip requirements requirements.txt project management',
+
+    entry_points={
+        "console_scripts": [
+            "mpip = mpip:main",
+        ],
+    },
+    install_requires=["fire", "rich", "execution-pipeline"],
+)
